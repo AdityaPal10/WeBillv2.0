c60682e5f4b9a15d81f9cad7b9ff8cfc7510ed9e
Aditya Pal <pal.aditya.ap@gmail.com>
2022-04-14 00:36:42 -0400

    friends front end implementation

---
+++ app/src/main/java/com/teamblue/WeBillv2/view/fragments/FriendChildFragment.java
@@ -0,0 +1,25 @@
+package com.teamblue.WeBillv2.view.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.teamblue.WeBillv2.R;
+
+public class FriendChildFragment  extends Fragment {
+
+    public FriendChildFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friends, container, false);
+    }
+}
--- app/src/main/java/com/teamblue/WeBillv2/view/fragments/FriendFragment.java
+++ app/src/main/java/com/teamblue/WeBillv2/view/fragments/FriendFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.teamblue.WeBillv2.R;
 
@@ -14,7 +15,7 @@ import com.teamblue.WeBillv2.R;
 /**
  * friend fragment subclass.
  */
-public class FriendFragment extends Fragment {
+public class FriendFragment<addFriendBttn> extends Fragment {
 
     public FriendFragment() {
         // Required empty public constructor
@@ -27,4 +28,86 @@ public class FriendFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_friends, container, false);
     }
+    public class FragmentInsideFragmentTestActivity extends Activity {
+
+        private Button addFriendBttn;
+        addFriendBttn = (Button) findViewById(R.id.btn_addFriends);
+
+
+        /** Called when the activity is first created. */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main);
+
+            button1 =(Button) this.findViewById(R.id.button1);
+            button1.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    onButtonClick(view);
+                }
+            });
+
+            button2 =(Button) this.findViewById(R.id.button2);
+            button2.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    onButtonClick(view);
+                }
+            });
+
+            button3 =(Button) this.findViewById(R.id.button3);
+            button3.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    onButtonClick(view);
+                }
+            });
+
+            button4 =(Button) this.findViewById(R.id.button4);
+            button4.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    onButtonClick(view);
+                }
+            });
+        }
+
+        public void onButtonClick(View v) {
+            Fragment fg;
+
+            switch (v.getId()) {
+                case R.id.button1:
+                    fg=FirstFragment.newInstance();
+                    replaceFragment(fg);
+                    break;
+                case R.id.button2:
+                    fg=SecondFragment.newInstance();
+                    replaceFragment(fg);
+                    break;
+                case R.id.button3:
+                    fg=FirstFragment.newInstance();
+                    replaceFragment(fg);
+                    break;
+                case R.id.button4:
+                    fg=SecondFragment.newInstance();
+                    replaceFragment(fg);
+                    break;
+            }
+        }
+
+        private void replaceFragment(Fragment newFragment) {
+            FragmentTransaction trasection = getFragmentManager().beginTransaction();
+
+            if(!newFragment.isAdded()) {
+                try {
+                    //FragmentTransaction trasection =
+                    getFragmentManager().beginTransaction();
+                    trasection.replace(R.id.linearLayout2, newFragment);
+                    trasection.addToBackStack(null);
+                    trasection.commit();
+                } catch (Exception e) {
+                    // TODO: handle exception
+                    // AppConstants.printLog(e.getMessage());
+                } else {
+                    trasection.show(newFragment);
+                }
+            }
+        }
 }
