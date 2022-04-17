8f54318b5ef40d92b81ca06fe02fa0608210a7ec
Aditya Pal <pal.aditya.ap@gmail.com>
2022-04-17 02:15:04 -0400

    friends

--- app/src/main/java/com/teamblue/WeBillv2/view/fragments/FriendFragment.java
+++ app/src/main/java/com/teamblue/WeBillv2/view/fragments/FriendFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.teamblue.WeBillv2.R;
@@ -33,25 +35,28 @@ public class FriendFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        return inflater.inflate(R.layout.fragment_friendsempty, container, false);
+
     }
+
     public class CustomAdapter extends BaseAdapter {
 
-        ArrayList<Integer> friendAvatar;
-        ArrayList<String> friendNames;
-        ArrayList<Integer> paymentDirection;
-        ArrayList<Double> paymentAmount;
-        ArrayList<Integer> buttonAction;
+        public ArrayList<Integer> friendAvatar;
+        public ArrayList<String> friendNames= new ArrayList<String>();
+        public ArrayList<Integer> paymentDirection;
+        public ArrayList<String> paymentAmount;
+        public ArrayList<Integer> buttonAction;
+        public Button btnAdd_Friends;
+
 
         /*
             1. make network call using retrofit
             2. from response received populate lists
             3. set context=acontext
              */
-        public CustomAdapter(Context acontext){
+        public CustomAdapter(Context acontext) {
             context = acontext;
         }
-
 
 
         @Override
@@ -74,23 +79,23 @@ public class FriendFragment extends Fragment {
         public View getView(int position, View view, ViewGroup viewGroup) {
 
             View row;
-            if (view==null){
-                LayoutInflater inflater=(LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 row = inflater.inflate(R.layout.friends_ledger_row, viewGroup, false);
+            } else {
+                row = view;
             }
-            else{
-                row=view;
-            }
-            Button bttnLedger=(Button) row.findViewById(R.id.bttnLedger);
-            TextView ledgerName=(TextView) row.findViewById(R.id.friendNameLedger);
-            TextView ledgerAmount=(TextView) row.findViewById(R.id.amountLedger);
-            TextView ledgerDirection=(TextView) row.findViewById(R.id.paymentDirectionLedger);
-            ImageView imageView = (ImageView)row.findViewById(R.id.imageViewLedger);
+            Button bttnLedger = (Button) row.findViewById(R.id.bttnLedger);
+            TextView ledgerName = (TextView) row.findViewById(R.id.friendNameLedger);
+            TextView ledgerAmount = (TextView) row.findViewById(R.id.amountLedger);
+            TextView ledgerDirection = (TextView) row.findViewById(R.id.paymentDirectionLedger);
+            ImageView imageView = (ImageView) row.findViewById(R.id.imageViewLedger);
+
 
             ledgerName.setText(friendNames.get(position));
             ledgerAmount.setText(String.valueOf(paymentAmount.get(position)));
-            ledgerDirection.setText(paymentDirection.get(position)==0?"Owes You:":"You Owe:");
-            bttnLedger.setText(paymentDirection.get(position)==0?"Remind":"Pay");
+            ledgerDirection.setText(paymentDirection.get(position) == 0 ? "Owes You:" : "You Owe:");
+            bttnLedger.setText(paymentDirection.get(position) == 0 ? "Remind" : "Pay");
             //imageView.setImageDrawable(R.);
 
             bttnLedger.setOnClickListener(new View.OnClickListener() {
@@ -102,5 +107,49 @@ public class FriendFragment extends Fragment {
 
             return row;
         }
+
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+
+            private TextView tv_friendName, tv_amount;
+            private LinearLayout title;
+
+            public MyViewHolder(View row) {
+                super(row);
+                title = (LinearLayout) row.findViewById(R.id.title);
+                tv_friendName = (TextView) row.findViewById(R.id.friendNameLedger);
+                tv_amount = (TextView) row.findViewById(R.id.amountLedger);
+            }
+        }
+
+
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.activity_row_data, parent, false);
+
+            return new MyViewHolder(itemView);
+        }
+
+
+        public void onBindViewHolder(MyViewHolder holder, final int position) {
+
+            holder.tv_friendName.setText(friendNames.get(position));
+            holder.tv_amount.setText(paymentAmount.get(position));
+            for (int i = 0; i < 5; i++) { // 5 is the count how many times you want to add textview
+                ViewGroup.LayoutParams lparams = new RecyclerView.LayoutParams(RecyclerView.LayoutParams.WRAP_CONTENT, RecyclerView.LayoutParams.WRAP_CONTENT);
+                TextView tv = new TextView(holder.title.getContext());
+                tv.setLayoutParams(lparams);
+                tv.setText("test = " + i);
+                holder.title.addView(tv);
+            }
+
+        }
+
+
+        public int getItemCount() {
+            return friendNames.size();
+        }
+
     }
 }
+
