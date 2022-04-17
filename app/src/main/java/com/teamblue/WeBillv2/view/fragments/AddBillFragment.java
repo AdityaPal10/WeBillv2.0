c8812ca43d29b22602a0d0414d1bf3674d280769
ZeqiWang <72681058+Jackie1010@users.noreply.github.com>
2022-04-17 01:20:48 -0400

    Skeleton of Add Bill Page

--- app/src/main/java/com/teamblue/WeBillv2/view/fragments/AddBillFragment.java
+++ app/src/main/java/com/teamblue/WeBillv2/view/fragments/AddBillFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import com.teamblue.WeBillv2.R;
 
@@ -25,6 +26,7 @@ public class AddBillFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
         return inflater.inflate(R.layout.fragment_add_bill, container, false);
     }
