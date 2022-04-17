2ccaf4d8fc276cd7eaf6da1ffb32474792283a60
ZeqiWang <72681058+Jackie1010@users.noreply.github.com>
2022-04-17 18:30:28 -0400

    Finished Add Bill Manually and Split Bill Manually

--- app/src/main/java/com/teamblue/WeBillv2/view/fragments/AddBillFragment.java
+++ app/src/main/java/com/teamblue/WeBillv2/view/fragments/AddBillFragment.java
@@ -1,21 +1,32 @@
 package com.teamblue.WeBillv2.view.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.teamblue.WeBillv2.R;
+import com.teamblue.WeBillv2.view.SplitBillActivity;
 
 
 /**
  *  addbill fragment subclass.
  */
 public class AddBillFragment extends Fragment {
+
+    private EditText edtActivityNameAddBill,edtTotalAmountAddBill,edtDateAddBill,edtAddressAddBill;
+    private Button btnEnterAddBill,btnScanBill;
 
     public AddBillFragment() {
         // Required empty public constructor
@@ -27,7 +38,69 @@ public class AddBillFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);//prevent keyboard pushing my view
+        View view = inflater.inflate(R.layout.fragment_add_bill, container, false);
 
-        return inflater.inflate(R.layout.fragment_add_bill, container, false);
+        edtActivityNameAddBill = (EditText) view.findViewById(R.id.edtActivityNameAddBill);
+        edtTotalAmountAddBill = (EditText) view.findViewById(R.id.edtTotalAmountAddBill);
+        edtDateAddBill = (EditText) view.findViewById(R.id.edtDateAddBill);
+        edtAddressAddBill = (EditText) view.findViewById(R.id.edtAddressAddBill);
+        btnEnterAddBill = (Button) view.findViewById(R.id.btnEnterAddBill);
+        btnScanBill = (Button) view.findViewById(R.id.btnScanBill);
+
+        btnEnterAddBill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Checking for no total amount empty error
+                if(TextUtils.isEmpty(edtActivityNameAddBill.getText().toString())  &&
+                        TextUtils.isEmpty(edtTotalAmountAddBill.getText().toString()) &&
+                        TextUtils.isEmpty(edtDateAddBill.getText().toString())  &&
+                        TextUtils.isEmpty(edtAddressAddBill.getText().toString())) {
+                    edtActivityNameAddBill.setError("Must Not Be Empty!");
+                    edtTotalAmountAddBill.setError("Must Not Be Empty!");
+                    edtDateAddBill.setError("Must Not Be Empty!");
+                    edtAddressAddBill.setError("Must Not Be Empty!");
+                    return;
+                }else{
+                    Bundle bundle = new Bundle();
+                    bundle.putString("BILL_ACTIVITY_NAME",edtActivityNameAddBill.getText().toString());
+                    bundle.putString("BILL_TOTAL_AMOUNT",edtTotalAmountAddBill.getText().toString());
+                    bundle.putString("BILL_DATE",edtDateAddBill.getText().toString());
+                    bundle.putString("BILL_ADDRESS",edtAddressAddBill.getText().toString());
+                    Intent gotoSplitBillActivity = new Intent(view.getContext(), SplitBillActivity.class);
+                    gotoSplitBillActivity.putExtras(bundle);
+                    startActivity(gotoSplitBillActivity);
+                }
+
+            }
+        });
+
+        btnScanBill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /********TODO: Start Scan Bill Activity here (not fragment!)********/
+            }
+        });
+
+//        edtTotalAmountAddBill.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                if (charSequence.toString().equals("")){
+//                    edtTotalAmountAddBill.setText("0");
+//                }
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        });
+
+
+        return view;
     }
 }
