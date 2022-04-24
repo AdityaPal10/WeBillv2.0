51f86eb3aff122f141ba390dcfd232eef9ccd306
liling-c <58797599+liling-c@users.noreply.github.com>
2022-04-24 17:29:25 -0400

    Added StripeAccountsActivity

---
+++ app/src/main/java/com/teamblue/WeBillv2/view/StripeAccountsActivity.java
@@ -0,0 +1,18 @@
+package com.teamblue.WeBillv2.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import com.teamblue.WeBillv2.R;
+
+public class StripeAccountsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
